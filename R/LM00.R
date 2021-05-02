2ceae96eb317fec1bc27bf17e0dffd46cb7a5e97
mehrdadsk <mehrdad.shokaz@gmail.com.com>
2021-05-03 00:26:35 +0200

    Making project to run

---
+++ R/LM00.R
@@ -0,0 +1,5 @@
+# Run all scripts ---------------------------------------------------------
+source(file = "../LearningMedium/R/LM01.R")
+source(file = "../LearningMedium/R/LM02.R")
+source(file = "../LearningMedium/R/LM03.R")
+dna_codons_to_aa(mk_codons(random_dna(133)))
